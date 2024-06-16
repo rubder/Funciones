e86c9cc282917e79bc203d21a362971df7fba180
Ruben Millan <165565025+rubder@users.noreply.github.com>
2024-06-16 15:06:37 -0400

    completado
    .

---
+++ 1 funcion/1_funcion.js
@@ -0,0 +1,16 @@
+// // función
+
+// function example(a, b, c){
+//     return a+b+c
+//     }
+//     alert(example(1, 2, 3));
+
+
+
+//función de expresión.
+
+const example = function(a, b, c) {
+    return a + b + c;
+};
+
+alert(example(5, 5, 5));
