7adee2cbfaffc8af176b74fa80f13c5756b76a20
Ruben Millan <165565025+rubder@users.noreply.github.com>
2024-06-16 16:49:57 -0400

    completado

--- 3 cambia color/assets/js/script.js
+++ 3 cambia color/assets/js/script.js
@@ -1,6 +1,27 @@
-function pintar(){
-ele.style.backgroundColor = 'yellow'
-}
-const ele = document.getElementById("ele1")
-ele.addEventListener("click", pintar);
+// // version original
+// function pintar(){
+// ele.style.backgroundColor = 'yellow'
+// }
+// const ele = document.getElementById("ele1")
+// ele.addEventListener("click", pintar);
 
+// // version 2.2
+// function pintar(elemento) {
+//     elemento.style.backgroundColor = 'yellow';
+// }
+
+// const ele = document.getElementById("ele1");
+// ele.addEventListener("click", function() {
+//     pintar(ele);
+// });
+
+
+
+function pintar(elemento, color = 'green') {
+    elemento.style.backgroundColor = color;
+}
+
+const ele = document.getElementById("ele1");
+ele.addEventListener("click", function() {
+    pintar(ele, 'yellow');
+});
