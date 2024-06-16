0dcb83d78c6bc7c12a387581da9600ef1fc4e98e
Ruben Millan <165565025+rubder@users.noreply.github.com>
2024-06-16 17:10:01 -0400

    completado

---
+++ 4 colores/assets/js/script.js
@@ -0,0 +1,20 @@
+const azul = document.getElementById("azul");
+const rojo = document.getElementById("rojo");
+const verde = document.getElementById("verde");
+const amarillo = document.getElementById("amarillo");
+
+azul.addEventListener("click", function() {
+    azul.style.backgroundColor = 'black';
+});
+
+rojo.addEventListener("click", function() {
+    rojo.style.backgroundColor = 'black';
+});
+
+verde.addEventListener("click", function() {
+    verde.style.backgroundColor = 'black';
+});
+
+amarillo.addEventListener("click", function() {
+    amarillo.style.backgroundColor = 'black';
+});
